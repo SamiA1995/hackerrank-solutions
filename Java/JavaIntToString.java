c5c6126c208f7a4a0ab8e3d812e0848fd13b9e2c
Sami Ahmed <emailsamiahmed@gmail.com>
2025-04-08 17:39:57 +0100

    Add JavaIntToString solution

---
+++ Java/JavaIntToString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.security.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}
