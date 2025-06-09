8b0349a5c173bfcf8b84345978d0de66f3e6150f
Sami Ahmed <emailsamiahmed@gmail.com>
2025-06-09 17:46:09 +0100

    Add Simple Array Sum solution

---
+++ Problem Solving/SimpleArraySum.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+    public static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+        for(int i = 0; i < ar.size(); i++) {
+            sum += ar.get(i);
+        }
+        return sum;
+    }
+
+}
+
+public class SimpleArraySum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
