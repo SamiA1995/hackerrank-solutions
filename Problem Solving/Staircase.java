5ba9bc30cba881885b893124ee5da4bcae61e67e
Sami Ahmed <emailsamiahmed@gmail.com>
2025-06-15 14:13:02 +0100

    Add Staircase solution

---
+++ Problem Solving/Staircase.java
@@ -0,0 +1,16 @@
+public class Staircase {
+
+    public static void main(String[] args) {
+        int n = 3;
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k < i+1; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+}
