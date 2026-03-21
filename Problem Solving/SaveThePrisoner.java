bd565737225f23a53ef69cabfff1fcb5e654e647
Sami Ahmed <emailsamiahmed@gmail.com>
2026-03-21 09:35:46 +0000

    Add Save the Prisoner solution

---
+++ Problem Solving/SaveThePrisoner.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class SaveThePrisoner {
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        int remainder = m % n;
+        for(int i = 0; i < remainder-1; i++) {
+            if(s == n) {
+                s = 1;
+            } else {
+                s++;
+            }
+        }
+        return s;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int n = Integer.parseInt(firstMultipleInput[0]);
+
+            int m = Integer.parseInt(firstMultipleInput[1]);
+
+            int s = Integer.parseInt(firstMultipleInput[2]);
+
+            int result = Result.saveThePrisoner(n, m, s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
